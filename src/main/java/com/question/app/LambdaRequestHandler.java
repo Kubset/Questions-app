53aadcca496efae1108fdeba90554bf19377e009
Kubset <jakub.setla@northgravity.com>
2020-02-25 16:45:55 +0100

    implement lambdaHandler and some minot features

--- src/main/java/com/question/app/LambdaRequestHandler.java
+++ src/main/java/com/question/app/LambdaRequestHandler.java
@@ -2,17 +2,39 @@ package com.question.app;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.question.app.scheduler.Scheduler;
+import com.question.app.service.BeanUtil;
+import com.question.app.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+import reactor.util.annotation.NonNull;
 
 import java.util.Map;
 
-
 public class LambdaRequestHandler implements RequestHandler<Map<String,String>, String> {
+
+    Scheduler scheduler;
+
+
+
+    public LambdaRequestHandler() {
+        String[] args = new String[0];
+//        SpringApplication.run(DemoApplication.class, args);
+        DemoApplication.main(args);
+        this.scheduler = BeanUtil.getBean(Scheduler.class);
+    }
 
     public String handleRequest(Map<String,String> input, Context context) {
 
+
         context.getLogger().log("Input: " + input);
-        System.out.println("TUUTTUUUT");
-        return "[{\"hello\":\"world\"}]";
+        context.getLogger().log(scheduler.toString());
+        scheduler.emailSender();
+        return "scheduler sent.";
     }
 
 }
--- src/main/java/com/question/app/scheduler/Scheduler.java
+++ src/main/java/com/question/app/scheduler/Scheduler.java
@@ -9,6 +9,7 @@ import com.question.app.service.EmailService;
 import com.question.app.service.QuestionService;
 import com.question.app.service.ScheduledEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
